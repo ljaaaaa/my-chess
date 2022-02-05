7ad65cd5b94e24a024df0b3dcaae75cd8eac1cc1
LiljaK <lilja.maria.kiiski@gmail.com>
2022-02-04 23:37:00 -0800

    Initial commit

---
+++ Piece.java
@@ -0,0 +1,26 @@
+public class Piece {
+
+	public class Pawn {
+
+	}
+
+	public class Rook {
+
+	}
+
+	public class Knight {
+
+	}
+
+	public class Bishop {
+
+	}
+
+	public class Queen {
+
+	}
+
+	public class King {
+
+	}
+}
