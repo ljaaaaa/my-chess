a0902e3337b0d754608af5935caf4c1d28b6c1e4
LiljaK <lilja.maria.kiiski@gmail.com>
2022-02-09 22:36:15 -0800

    Made pawns glow on mouse hover

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Point;
+import javax.swing.ImageIcon;
 import javax.swing.Timer;
 
 //Main Class
@@ -15,7 +17,15 @@ public class Main implements ActionListener{
 	}
 
 	public void actionPerformed(ActionEvent e){
-		
+		for (Pawn pawn : player.set.pawns){
+			Point mPoint = mouseListener.getMousePosition();
+                	if (mPoint.x != -1 && pawn.mouseOn(mPoint)){
+                                pawn.imageIcon = new ImageIcon("images/pawn2.png");
+                        } else {
+                                pawn.imageIcon = new ImageIcon("images/pawn.png");
+                	}
+		}
+		painter.repaint();
 	}
 
 	//Constructor
@@ -26,7 +36,7 @@ public class Main implements ActionListener{
 		mouseListener = new MyMouseListener(frame);
 		frame.setUp(this);
 	
-		Timer timer = new Timer(1000, this);
+		Timer timer = new Timer(10, this);
 		timer.start();
 	}
 }
--- Piece.java
+++ Piece.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
+import java.awt.Point;
 
 //Piece Class
 public class Piece{
 	int posX;
 	int posY;
+	final int ICONSIZE = 80;
 	final int GRIDSIZE = 8;
 
 	//Constructor
@@ -15,6 +17,17 @@ public class Piece{
 	//Move piece
 	public void move(int newX, int newY) throws IllegalMoveException{
 		
+	}
+
+	//If mouse is touching area
+	public boolean mouseOn(Point p){
+		if (p.x > posX*80 && p.x < posX*80+ICONSIZE 
+			&& p.y > posY*80-36 && p.y < posY*80+ICONSIZE+38){
+			
+			return true;
+		}
+
+		return false;
 	}
 
 	//Check if move is valid
