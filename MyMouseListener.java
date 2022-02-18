51a08739d3db25e5f1f7148b963ab2a6a9651766
LiljaK <lilja.maria.kiiski@gmail.com>
2022-02-18 11:47:09 -0800

    Pawn moving works! (Note ArrayIndexOutOfBoundsException can happen once Pawn comes to range of game board

--- MyMouseListener.java
+++ MyMouseListener.java
@@ -15,7 +15,6 @@ public class MyMouseListener implements MouseListener{
 	public MyMouseListener(Main main){
 		this.main = main;
 		state = State.NO_SELECTION;
-		main.grid.printGrid();
 	}
 	
 	@Override
@@ -32,20 +31,14 @@ public class MyMouseListener implements MouseListener{
 
         @Override
         public void mouseClicked(MouseEvent e) {
-		Tile selected = findSelectedTile(e.getPoint());;
+		Tile selected = null;
 
-		System.out.println(selected);
-
-		if (selected != null){
-			selected.setSelected(true);
-		}
-
-		/*
 		switch (state){
 			//No chess piece selected
 			case NO_SELECTION: 
 				//Selected tile and grid points
 				selected = findSelectedTile(e.getPoint());
+				updateTiles(e.getPoint());
 
 				//Chess piece selected
 				if (selected instanceof Piece){
@@ -61,40 +54,35 @@ public class MyMouseListener implements MouseListener{
 			//Chess piece selected previously
 			case SELECTED_PIECE: 
 				Tile oldSelected = getSelectedTile();
-                       		selected = findSelectedTile(e.getPoint());
+				updateTiles(e.getPoint());
+				selected = findSelectedTile(e.getPoint());
+				
+				if (oldSelected instanceof Piece && ((Piece)oldSelected).possibleMoves().contains(selected)){
+					((Piece)oldSelected).move(selected.posX, selected.posY);
 
-				if (selected.possible && (oldSelected instanceof Piece)){
-                                	((Piece)oldSelected).move(selected.posX, selected.posY);
+				} else if (selected instanceof Piece){
+					//Highlight possible moves
+                                        ArrayList<Tile> possibles = ((Piece)selected).possibleMoves();
+                                       	highlightPossibles(possibles);
 				}
+				updateTiles(e.getPoint());
 				state = State.NO_SELECTION;
 				break;
 		}
-		*/
 	}
 
-	//Returns selected piece by mouse
+	//Return selected tile
 	public Tile findSelectedTile(Point mousePoint){
-		Tile selected = null;
-		
 		for (int x = 0; x < main.grid.grid.length; x++){
                         for (int y = 0; y < main.grid.grid[x].length; y++){
-				Tile tile = main.grid.grid[x][y];
+                                Tile tile = main.grid.grid[x][y];
 
-				//Mouse on
-				if (tile.mouseOn(mousePoint)){
-					tile.setSelected(true);
-					tile.possible = false;
-					selected = tile;
-					System.out.println("bok");
-			
-				//Mouse not on
-				} else {
-					tile.setSelected(false);
-					tile.possible = false;
-				}
-			}
-		}
-		return selected;
+                                if (tile.mouseOn(mousePoint)){
+                                       	return tile;
+                               	}
+                        }
+                }
+		return null;
 	}
 
 	//Get the currently selected piece in grid
@@ -109,6 +97,25 @@ public class MyMouseListener implements MouseListener{
                         }
                 }
 		return null;
+	}
+
+	//Update tile selected and possible statuses
+	public void updateTiles(Point mousePoint){
+		for (int x = 0; x < main.grid.grid.length; x++){
+                        for (int y = 0; y < main.grid.grid[x].length; y++){
+				Tile tile = main.grid.grid[x][y];
+
+				//Mouse on
+				if (tile.mouseOn(mousePoint)){
+					tile.setSelected(true);
+					tile.possible = false;
+				//Mouse not on
+				} else {
+					tile.setSelected(false);
+					tile.possible = false;
+				}
+			}
+		}
 	}
 
 	//Highlight possibles
