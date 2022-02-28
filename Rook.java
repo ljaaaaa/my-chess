18676394b8c102230b4f92c6ce5ecf2c9034ca05
LiljaK <lilja.maria.kiiski@gmail.com>
2022-02-27 19:26:14 -0800

    Rook movement working (no bugs yet) - next up... Knight?

--- Rook.java
+++ Rook.java
@@ -15,6 +15,67 @@ public class Rook extends Piece{
 	@Override
         public ArrayList<Tile> possibleMoves(){
 		ArrayList<Tile> possibles = new ArrayList<>(); 
+		int add = 1;
+
+		//Up
+		while (posY-add >= 0){	
+			possibles.add(grid.grid[posX][posY-add]);
+
+			//Has hit a piece, stop
+			if (grid.grid[posX][posY-add] instanceof Piece){
+				//If piece is of own color, don't eat
+				if (((Piece)grid.grid[posX][posY-add]).color == color){
+					possibles.remove(possibles.size()-1);
+				}
+				break;
+			}
+			add++;
+		}
+		add = 1;
+
+		//Down
+		while (posY+add < 8){
+                        possibles.add(grid.grid[posX][posY+add]);
+
+                        if (grid.grid[posX][posY+add] instanceof Piece){
+				//If piece is of own color, don't eat
+                                if (((Piece)grid.grid[posX][posY+add]).color == color){
+                                        possibles.remove(possibles.size()-1);
+                                }
+				break;
+                        }
+                        add++;
+                }
+		add = 1;
+
+		//Left
+		while (posX-add >= 0 ){
+			possibles.add(grid.grid[posX-add][posY]);
+
+			if (grid.grid[posX-add][posY] instanceof Piece){
+				//If piece is of own color, don't eat
+                                if (((Piece)grid.grid[posX-add][posY]).color == color){
+                                        possibles.remove(possibles.size()-1);
+                                }
+				break;
+			}
+			add++;
+		}
+		add = 1;
+
+		//Right
+                while (posX+add < 8){
+                        possibles.add(grid.grid[posX+add][posY]);
+
+                        if (grid.grid[posX+add][posY] instanceof Piece){
+				//If piece is of own color, don't eat
+                                if (((Piece)grid.grid[posX+add][posY]).color == color){
+                                        possibles.remove(possibles.size()-1);
+                                }
+				break;
+                        }
+                        add++;
+                }
 
 		return possibles;
         }
