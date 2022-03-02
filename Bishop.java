4f6266d3db9e806364649c95347958fe620d558d
LiljaK <lilja.maria.kiiski@gmail.com>
2022-03-02 12:35:22 -0800

    Added possibles for pawn - need to test them out

--- Bishop.java
+++ Bishop.java
@@ -15,6 +15,69 @@ public class Bishop extends Piece{
 
 	@Override
         protected void setPossibles(){
-
+		possibles[0][0] = new CList(new C(1, 1), new C(2, 2), new C(3, 3), new C(4, 4), new C(5, 5), new C(6, 6), new C(7, 7));
+		possibles[0][1] = new CList(new C(1, 0), new C(1, 2), new C(2, 3), new C(3, 4), new C(4, 5), new C(5, 6), new C(6, 7));
+		possibles[0][2] = new CList(new C(1, 1), new C(2, 0), new C(1, 3), new C(2, 4), new C(3, 5), new C(4, 6), new C(5, 7));
+		possibles[0][3] = new CList(new C(1, 2), new C(2, 1), new C(3, 0), new C(1, 4), new C(2, 5), new C(3, 6), new C(4, 7));
+		possibles[0][4] = new CList(new C(1, 3), new C(2, 2), new C(3, 1), new C(4, 0), new C(1, 5), new C(2, 6), new C(3, 7));
+		possibles[0][5] = new CList(new C(1, 4), new C(2, 3), new C(3, 2), new C(4, 1), new C(5, 0), new C(1, 6), new C(2, 7));
+		possibles[0][6] = new CList(new C(1, 5), new C(2, 4), new C(3, 3), new C(4, 2), new C(5, 1), new C(6, 0), new C(1, 7));
+		possibles[0][7] = new CList(new C(1, 6), new C(2, 5), new C(3, 4), new C(4, 3), new C(5, 2), new C(6, 1), new C(7, 0));
+		possibles[1][0] = new CList(new C(0, 1), new C(2, 1), new C(3, 2), new C(4, 3), new C(5, 4), new C(6, 5), new C(7, 6));
+		possibles[1][1] = new CList(new C(0, 0), new C(2, 0), new C(0, 2), new C(2, 2), new C(3, 3), new C(4, 4), new C(5, 5), new C(6, 6), new C(7, 7));
+		possibles[1][2] = new CList(new C(0, 1), new C(2, 1), new C(3, 0), new C(0, 3), new C(2, 3), new C(3, 4), new C(4, 5), new C(5, 6), new C(6, 7));
+		possibles[1][3] = new CList(new C(0, 2), new C(2, 2), new C(3, 1), new C(4, 0), new C(0, 4), new C(2, 4), new C(3, 5), new C(4, 6), new C(5, 7));
+		possibles[1][4] = new CList(new C(0, 3), new C(2, 3), new C(3, 2), new C(4, 1), new C(5, 0), new C(0, 5), new C(2, 5), new C(3, 6), new C(4, 7));
+		possibles[1][5] = new CList(new C(0, 4), new C(2, 4), new C(3, 3), new C(4, 2), new C(5, 1), new C(6, 0), new C(0, 6), new C(2, 6), new C(3, 7));
+		possibles[1][6] = new CList(new C(0, 5), new C(2, 5), new C(3, 4), new C(4, 3), new C(5, 2), new C(6, 1), new C(7, 0), new C(0, 7), new C(2, 7));
+		possibles[1][7] = new CList(new C(0, 6), new C(2, 6), new C(3, 5), new C(4, 4), new C(5, 3), new C(6, 2), new C(7, 1));
+		possibles[2][0] = new CList(new C(1, 1), new C(0, 2), new C(3, 1), new C(4, 2), new C(5, 3), new C(6, 4), new C(7, 5));
+		possibles[2][1] = new CList(new C(1, 0), new C(3, 0), new C(1, 2), new C(0, 3), new C(3, 2), new C(4, 3), new C(5, 4), new C(6, 5), new C(7, 6));
+		possibles[2][2] = new CList(new C(1, 1), new C(0, 0), new C(3, 1), new C(4, 0), new C(1, 3), new C(0, 4), new C(3, 3), new C(4, 4), new C(5, 5), new C(6, 6), new C(7, 7));
+		possibles[2][3] = new CList(new C(1, 2), new C(0, 1), new C(3, 2), new C(4, 1), new C(5, 0), new C(1, 4), new C(0, 5), new C(3, 4), new C(4, 5), new C(5, 6), new C(6, 7));
+		possibles[2][4] = new CList(new C(1, 3), new C(0, 2), new C(3, 3), new C(4, 2), new C(5, 1), new C(6, 0), new C(1, 5), new C(0, 6), new C(3, 5), new C(4, 6), new C(5, 7));
+		possibles[2][5] = new CList(new C(1, 4), new C(0, 3), new C(3, 4), new C(4, 3), new C(5, 2), new C(6, 1), new C(7, 0), new C(1, 6), new C(0, 7), new C(3, 6), new C(4, 7));
+		possibles[2][6] = new CList(new C(1, 5), new C(0, 4), new C(3, 5), new C(4, 4), new C(5, 3), new C(6, 2), new C(7, 1), new C(1, 7), new C(3, 7));
+		possibles[2][7] = new CList(new C(1, 6), new C(0, 5), new C(3, 6), new C(4, 5), new C(5, 4), new C(6, 3), new C(7, 2));
+		possibles[3][0] = new CList(new C(2, 1), new C(1, 2), new C(0, 3), new C(4, 1), new C(5, 2), new C(6, 3), new C(7, 4));
+		possibles[3][1] = new CList(new C(2, 0), new C(4, 0), new C(2, 2), new C(1, 3), new C(0, 4), new C(4, 2), new C(5, 3), new C(6, 4), new C(7, 5));
+		possibles[3][2] = new CList(new C(2, 1), new C(1, 0), new C(4, 1), new C(5, 0), new C(2, 3), new C(1, 4), new C(0, 5), new C(4, 3), new C(5, 4), new C(6, 5), new C(7, 6));
+		possibles[3][3] = new CList(new C(2, 2), new C(1, 1), new C(0, 0), new C(4, 2), new C(5, 1), new C(6, 0), new C(2, 4), new C(1, 5), new C(0, 6), new C(4, 4), new C(5, 5), new C(6, 6), new C(7, 7));
+		possibles[3][4] = new CList(new C(2, 3), new C(1, 2), new C(0, 1), new C(4, 3), new C(5, 2), new C(6, 1), new C(7, 0), new C(2, 5), new C(1, 6), new C(0, 7), new C(4, 5), new C(5, 6), new C(6, 7));
+		possibles[3][5] = new CList(new C(2, 4), new C(1, 3), new C(0, 2), new C(4, 4), new C(5, 3), new C(6, 2), new C(7, 1), new C(2, 6), new C(1, 7), new C(4, 6), new C(5, 7));
+		possibles[3][6] = new CList(new C(2, 5), new C(1, 4), new C(0, 3), new C(4, 5), new C(5, 4), new C(6, 3), new C(7, 2), new C(2, 7), new C(4, 7));
+		possibles[3][7] = new CList(new C(2, 6), new C(1, 5), new C(0, 4), new C(4, 6), new C(5, 5), new C(6, 4), new C(7, 3));
+		possibles[4][0] = new CList(new C(3, 1), new C(2, 2), new C(1, 3), new C(0, 4), new C(5, 1), new C(6, 2), new C(7, 3));
+		possibles[4][1] = new CList(new C(3, 0), new C(5, 0), new C(3, 2), new C(2, 3), new C(1, 4), new C(0, 5), new C(5, 2), new C(6, 3), new C(7, 4));
+		possibles[4][2] = new CList(new C(3, 1), new C(2, 0), new C(5, 1), new C(6, 0), new C(3, 3), new C(2, 4), new C(1, 5), new C(0, 6), new C(5, 3), new C(6, 4), new C(7, 5));
+		possibles[4][3] = new CList(new C(3, 2), new C(2, 1), new C(1, 0), new C(5, 2), new C(6, 1), new C(7, 0), new C(3, 4), new C(2, 5), new C(1, 6), new C(0, 7), new C(5, 4), new C(6, 5), new C(7, 6));
+		possibles[4][4] = new CList(new C(3, 3), new C(2, 2), new C(1, 1), new C(0, 0), new C(5, 3), new C(6, 2), new C(7, 1), new C(3, 5), new C(2, 6), new C(1, 7), new C(5, 5), new C(6, 6), new C(7, 7));
+		possibles[4][5] = new CList(new C(3, 4), new C(2, 3), new C(1, 2), new C(0, 1), new C(5, 4), new C(6, 3), new C(7, 2), new C(3, 6), new C(2, 7), new C(5, 6), new C(6, 7));
+		possibles[4][6] = new CList(new C(3, 5), new C(2, 4), new C(1, 3), new C(0, 2), new C(5, 5), new C(6, 4), new C(7, 3), new C(3, 7), new C(5, 7));
+		possibles[4][7] = new CList(new C(3, 6), new C(2, 5), new C(1, 4), new C(0, 3), new C(5, 6), new C(6, 5), new C(7, 4));
+		possibles[5][0] = new CList(new C(4, 1), new C(3, 2), new C(2, 3), new C(1, 4), new C(0, 5), new C(6, 1), new C(7, 2));
+		possibles[5][1] = new CList(new C(4, 0), new C(6, 0), new C(4, 2), new C(3, 3), new C(2, 4), new C(1, 5), new C(0, 6), new C(6, 2), new C(7, 3));
+		possibles[5][2] = new CList(new C(4, 1), new C(3, 0), new C(6, 1), new C(7, 0), new C(4, 3), new C(3, 4), new C(2, 5), new C(1, 6), new C(0, 7), new C(6, 3), new C(7, 4));
+		possibles[5][3] = new CList(new C(4, 2), new C(3, 1), new C(2, 0), new C(6, 2), new C(7, 1), new C(4, 4), new C(3, 5), new C(2, 6), new C(1, 7), new C(6, 4), new C(7, 5));
+		possibles[5][4] = new CList(new C(4, 3), new C(3, 2), new C(2, 1), new C(1, 0), new C(6, 3), new C(7, 2), new C(4, 5), new C(3, 6), new C(2, 7), new C(6, 5), new C(7, 6));
+		possibles[5][5] = new CList(new C(4, 4), new C(3, 3), new C(2, 2), new C(1, 1), new C(0, 0), new C(6, 4), new C(7, 3), new C(4, 6), new C(3, 7), new C(6, 6), new C(7, 7));
+		possibles[5][6] = new CList(new C(4, 5), new C(3, 4), new C(2, 3), new C(1, 2), new C(0, 1), new C(6, 5), new C(7, 4), new C(4, 7), new C(6, 7));
+		possibles[5][7] = new CList(new C(4, 6), new C(3, 5), new C(2, 4), new C(1, 3), new C(0, 2), new C(6, 6), new C(7, 5));
+		possibles[6][0] = new CList(new C(5, 1), new C(4, 2), new C(3, 3), new C(2, 4), new C(1, 5), new C(0, 6), new C(7, 1));
+		possibles[6][1] = new CList(new C(5, 0), new C(7, 0), new C(5, 2), new C(4, 3), new C(3, 4), new C(2, 5), new C(1, 6), new C(0, 7), new C(7, 2));
+		possibles[6][2] = new CList(new C(5, 1), new C(4, 0), new C(7, 1), new C(5, 3), new C(4, 4), new C(3, 5), new C(2, 6), new C(1, 7), new C(7, 3));
+		possibles[6][3] = new CList(new C(5, 2), new C(4, 1), new C(3, 0), new C(7, 2), new C(5, 4), new C(4, 5), new C(3, 6), new C(2, 7), new C(7, 4));
+		possibles[6][4] = new CList(new C(5, 3), new C(4, 2), new C(3, 1), new C(2, 0), new C(7, 3), new C(5, 5), new C(4, 6), new C(3, 7), new C(7, 5));
+		possibles[6][5] = new CList(new C(5, 4), new C(4, 3), new C(3, 2), new C(2, 1), new C(1, 0), new C(7, 4), new C(5, 6), new C(4, 7), new C(7, 6));
+		possibles[6][6] = new CList(new C(5, 5), new C(4, 4), new C(3, 3), new C(2, 2), new C(1, 1), new C(0, 0), new C(7, 5), new C(5, 7), new C(7, 7));
+		possibles[6][7] = new CList(new C(5, 6), new C(4, 5), new C(3, 4), new C(2, 3), new C(1, 2), new C(0, 1), new C(7, 6));
+		possibles[7][0] = new CList(new C(6, 1), new C(5, 2), new C(4, 3), new C(3, 4), new C(2, 5), new C(1, 6), new C(0, 7));
+		possibles[7][1] = new CList(new C(6, 0), new C(6, 2), new C(5, 3), new C(4, 4), new C(3, 5), new C(2, 6), new C(1, 7));
+		possibles[7][2] = new CList(new C(6, 1), new C(5, 0), new C(6, 3), new C(5, 4), new C(4, 5), new C(3, 6), new C(2, 7));
+		possibles[7][3] = new CList(new C(6, 2), new C(5, 1), new C(4, 0), new C(6, 4), new C(5, 5), new C(4, 6), new C(3, 7));
+		possibles[7][4] = new CList(new C(6, 3), new C(5, 2), new C(4, 1), new C(3, 0), new C(6, 5), new C(5, 6), new C(4, 7));
+		possibles[7][5] = new CList(new C(6, 4), new C(5, 3), new C(4, 2), new C(3, 1), new C(2, 0), new C(6, 6), new C(5, 7));
+		possibles[7][6] = new CList(new C(6, 5), new C(5, 4), new C(4, 3), new C(3, 2), new C(2, 1), new C(1, 0), new C(6, 7));
+		possibles[7][7] = new CList(new C(6, 6), new C(5, 5), new C(4, 4), new C(3, 3), new C(2, 2), new C(1, 1), new C(0, 0));
 	}
 }
--- Grid.java
+++ Grid.java
@@ -7,8 +7,8 @@ public class Grid {
 		grid = new Tile[8][8];
 		setBaseGrid();
 		//initPawns();
-		//initBishops();
-		initRooks();
+		initBishops();
+		//initRooks();
 		//initKnights();
 	}
 
@@ -21,38 +21,35 @@ public class Grid {
 		}
 	}
 
-	//Initialize pawns onto grid
 	private void initPawns(){        
 		for (int x = 0; x < grid.length; x++){
-	//		grid[x][1] = new Pawn(x, 1, this, 'w');
+			grid[x][1] = new Pawn(x, 1, 'w');
 		}
 
 		for (int x = 0; x < grid.length; x++){
-        //                grid[x][6] = new Pawn(x, 6, this, 'b');
+                        grid[x][6] = new Pawn(x, 6,  'b');
                 }
 	}
 
-	//Initialize bishops onto grid
         private void initBishops(){
-        //        grid[1][0] = new Bishop(1, 0, this, 'w');
-	//	grid[6][0] = new Bishop(6, 0, this, 'w');
-	//	grid[1][7] = new Bishop(1, 7, this, 'b');
-	//	grid[6][7] = new Bishop(6, 7, this, 'b');	
+                grid[1][0] = new Bishop(1, 0,  'w');
+//		grid[6][0] = new Bishop(6, 0,  'w');
+//		grid[1][7] = new Bishop(1, 7,  'b');
+//		grid[6][7] = new Bishop(6, 7,  'b');	
 	}
 
-	//Initialize rooks onto grid
         private void initRooks(){
                 grid[0][0] = new Rook(0, 0, 'w');
-        //        grid[7][0] = new Rook(7, 0, this, 'w');
-        //        grid[0][7] = new Rook(0, 7, this, 'b');
-        //        grid[7][7] = new Rook(7, 7, this, 'b');
+                grid[7][0] = new Rook(7, 0, 'w');
+                grid[0][7] = new Rook(0, 7, 'b');
+                grid[7][7] = new Rook(7, 7, 'b');
         }
 
 	private void initKnights(){
-	//	grid[2][0] = new Knight(2, 0, this, 'w');
-        //        grid[5][0] = new Knight(5, 0, this, 'w');
-        //        grid[2][7] = new Knight(2, 7, this, 'b');
-        //        grid[5][7] = new Knight(5, 7, this, 'b');
+		grid[2][0] = new Knight(2, 0, 'w');
+                grid[5][0] = new Knight(5, 0, 'w');
+                grid[2][7] = new Knight(2, 7, 'b');
+                grid[5][7] = new Knight(5, 7, 'b');
 	}
 
 	//Print grid, for debugging
