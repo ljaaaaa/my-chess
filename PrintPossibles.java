1f1572b81b7ce335aecb6aa8dd0c93306ee29754
LiljaK <lilja.maria.kiiski@gmail.com>
2022-03-04 23:39:20 -0800

    Revert "Added possibles for pawn - need to test them out"
    This reverts commit 4f6266d3db9e806364649c95347958fe620d558d.

--- PrintPossibles.java
+++ PrintPossibles.java
@@ -3,26 +3,16 @@ import java.util.ArrayList;
 //Print Possibles Class
 public class PrintPossibles {
 
-	public static void main(String[] args){
-		for (int x = 0; x < 8; x++){
-			for (int y = 0; y < 8; y++){
-				Pawn p = new Pawn(x, y, 'w');
-				ArrayList<Tile> possibles = new PrintPossibles().pawnPossibleMoves(x, y, new Grid(), 'b', Pawn.Direction.UP);
-				printPossibles(p, possibles);
-			}
-		}
-	}
-
         public static void printPossibles(Piece piece, ArrayList<Tile> possibles){
                 System.out.print("possibles[" + piece.x + "][" + piece.y + "] = new CList(");
                 for (int x = 0; x < possibles.size(); x++){
                         if (x == possibles.size()-1){
-                        System.out.print("new C(" + possibles.get(x).x + ", " + possibles.get(x).y + ")");
+                        System.out.print("new C(" + possibles.get(x).x + ", " + possibles.get(x).y + "));");
                         } else {
                                 System.out.print("new C(" + possibles.get(x).x + ", " + possibles.get(x).y + "), ");
                         }
                 }
-                System.out.print(");\n");
+                System.out.println();
         }
 
 	public ArrayList<Tile> knightPossibleMoves(int posX, int posY, Grid grid){
@@ -74,13 +64,30 @@ public class PrintPossibles {
 		//Diagonal up left
 		while (posY-add >= 0 && posX-add >= 0){
 			possibles.add(grid.grid[posX-add][posY-add]);
+
+			//Has hit a piece, stop
+			if (grid.grid[posX-add][posY-add] instanceof Piece){
+				//If piece is of own color, don't eat
+				if (((Piece)grid.grid[posX-add][posY-add]).color == color){
+					possibles.remove(possibles.size()-1);
+				}
+				break;
+			}
 			add++;
 		}
 		add = 1;
 
 		//Diagonal up right
 		while (posY-add >= 0 && posX+add < 8){
-                        possibles.add(grid.grid[posX+add][posY-add]); 
+                        possibles.add(grid.grid[posX+add][posY-add]);
+
+                        if (grid.grid[posX+add][posY-add] instanceof Piece){
+				//If piece is of own color, don't eat
+                                if (((Piece)grid.grid[posX+add][posY-add]).color == color){
+                                        possibles.remove(possibles.size()-1);
+                                }
+				break;
+                        }
                         add++;
                 }
 		add = 1;
@@ -89,13 +96,28 @@ public class PrintPossibles {
 		while (posY+add < 8 && posX-add >= 0 ){
 			possibles.add(grid.grid[posX-add][posY+add]);
 
+			if (grid.grid[posX-add][posY+add] instanceof Piece){
+				//If piece is of own color, don't eat
+                                if (((Piece)grid.grid[posX-add][posY+add]).color == color){
+                                        possibles.remove(possibles.size()-1);
+                                }
+				break;
+			}
 			add++;
 		}
 		add = 1;
 
 		//Diagonal down right
                 while (posY+add < 8 && posX+add < 8){
-			possibles.add(grid.grid[posX+add][posY+add]);
+                        possibles.add(grid.grid[posX+add][posY+add]);
+
+                        if (grid.grid[posX+add][posY+add] instanceof Piece){
+				//If piece is of own color, don't eat
+                                if (((Piece)grid.grid[posX+add][posY+add]).color == color){
+                                        possibles.remove(possibles.size()-1);
+                                }
+				break;
+                        }
                         add++;
                 }
 
@@ -109,44 +131,45 @@ public class PrintPossibles {
 			//Piece is going down
 			case DOWN:
 				//Next tile
-               			if (posY+1 < 8){
+               			if (posY+1 < 8 && !(grid.grid[posX][posY+1] instanceof Piece)){
                         		possibles.add(grid.grid[posX][posY+1]);
 
 					//Next two tiles
-                			if (posY == 1){
+                			if (posY == 1 && !(grid.grid[posX][posY+2] instanceof Piece)){
                                 		possibles.add(grid.grid[posX][posY+2]);
 					}
 				}
 
 				//Diagonal tile
-				//if (posY+1 < 8 && posX+1 < 8){
-                                //        possibles.add(grid.grid[posX+1][posY+1]);
+				if (posY+1 < 8 && posX+1 < 8 && (grid.grid[posX+1][posY+1] instanceof Piece) && ((Piece)grid.grid[posX+1][posY+1]).color != color){
+                                        possibles.add(grid.grid[posX+1][posY+1]);
 
 				//Diagonal tile
-				//} if (posY+1 < 8 && posX-1 >= 0){
-                                //        possibles.add(grid.grid[posX-1][posY+1]);
-				//}
+				} if (posY+1 < 8 && posX-1 >= 0 && (grid.grid[posX-1][posY+1] instanceof Piece) && ((Piece)grid.grid[posX-1][posY+1]).color != color){
+                                        possibles.add(grid.grid[posX-1][posY+1]);
+				}
+
 				break;
 
 			//Piece is going up
 			case UP:
 				//Next tile
-                                if (posY-1 >= 0){
+                                if (posY-1 >= 0 && !(grid.grid[posX][posY-1] instanceof Piece)){
                                         possibles.add(grid.grid[posX][posY-1]);
 
                                 	//Next two tiles
-                                	if (posY == 6){
+                                	if (posY == 6 && !(grid.grid[posX][posY-2] instanceof Piece)){
                                         	possibles.add(grid.grid[posX][posY-2]);
 					}
 				}
 				//Diagonal tile
-				//if (posY-1 >= 0 && posX+1 < 8){
-                                //        possibles.add(grid.grid[posX+1][posY-1]);
+				if (posY-1 >= 0 && posX+1 < 8 && (grid.grid[posX+1][posY-1] instanceof Piece) && ((Piece)grid.grid[posX+1][posY-1]).color != color){
+                                        possibles.add(grid.grid[posX+1][posY-1]);
 
 				//Diagonal tile
-				//} if (posY-1 >= 0 && posX-1 >= 0){
-                                //        possibles.add(grid.grid[posX-1][posY-1]);
-                                //}
+				} if (posY-1 >= 0 && posX-1 >= 0 && (grid.grid[posX-1][posY-1] instanceof Piece) && ((Piece)grid.grid[posX-1][posY-1]).color != color){
+                                        possibles.add(grid.grid[posX-1][posY-1]);
+                                }
 
 				break;
 		}
