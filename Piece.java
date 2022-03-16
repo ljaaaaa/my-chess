678b64e0ee56500a4bcd5b33911fc78e7828b77f
LiljaK <lilja.maria.kiiski@gmail.com>
2022-03-16 11:34:06 -0700

    Yaaay - another bug

--- Piece.java
+++ Piece.java
@@ -128,7 +128,6 @@ public class Piece extends Tile{
 		Set thisSet = color == 'w' ? dummy.setW : dummy.setB;
 		Set otherSet = color == 'w' ? dummy.setB : dummy.setW;
 
-		System.out.println("checking...");
 		if (thisSet.kingCanBeEaten(grid, otherSet)){
 			System.out.println( newX + " : " + newY + " will put own king in danger");
 			return true;
